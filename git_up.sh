ef2df7a025548723b01c29b0f88870e5af22812f
pablinn <penalve.sergio@yahoo.com.ar>
2018-06-18 19:17:18 -0300

    Segunda revision

--- git_up.sh
+++ git_up.sh
@@ -2,7 +2,7 @@
 git add *
 git commit -m "Segunda revision"
 git status
-git rm *
+git rm 
 git commit -m "Segunda revision"
-git remote add origin git@github.com:pablinn/ceh.git
+#git remote add origin git@github.com:pablinn/ceh.git
 git push -u -f origin master
